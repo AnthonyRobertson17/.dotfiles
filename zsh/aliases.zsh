57903507cf4c67c99008716d9099bb091f62b400
Anthony Robertson <arob@hey.com>
2021-12-22 16:36:14 -0500

    remove out of date aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,3 @@
-alias vi=/usr/local/bin/vim
-alias vim=/usr/local/bin/vim
-alias editalias='$EDITOR ~/.oh-my-zsh/custom/aliases.zsh'
 alias editzsh='$EDITOR ~/.zshrc'
 alias editvim='$EDITOR ~/.vimrc'
 alias refresh='source ~/.zshrc'
@@ -27,12 +24,3 @@ alias dcr='docker-compose restart'
 alias dcu='docker-compose up'
 alias dcud='docker-compose up -d'
 alias dc='docker-compose'
-
-alias activate='source venv/bin/activate'
-
-alias stagingdb01tunnel='gcloud compute start-iap-tunnel mysql-c3-n3 3306 --local-host-port=localhost:3308 --zone us-east1-c'
-alias stagingdb02tunnel='gcloud compute start-iap-tunnel mysql-c4-n3 3306 --local-host-port=localhost:3307 --zone us-east1-c'
-alias proddbtunnel='gcloud compute start-iap-tunnel proxysql-dev-access-us-east1-c-n0 3306 --local-host-port=localhost:3309 --project freshbooks-prod --zone us-east1-c'
-alias stagingdbtunnel='gcloud compute start-iap-tunnel proxysql-dev-access-us-east1-c-n0 3306 --local-host-port=localhost:3308 --project freshbooks-staging --zone us-east1-c'
-
-alias up-billing='docker-compose up -d core masterlock_billing_aria_worker cardcaptor commandcenter'
